61bd62a45f4a190085ad1cfbe386b7a6737dccf2
Pelagoss <pelagoss@outlook.fr>
2021-01-07 17:22:47 +0100

    commit de zeub

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flaskwebgui import FlaskUI   # get the FlaskUI class
 import wikipedia
+import json
 
 
 app = Flask(__name__)
@@ -9,17 +10,44 @@ ui = FlaskUI(app)                 # feed the parameters
 
 # do your logic as usual in Flask
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    wikipedia.set_lang("fr")
-    page_py = wikipedia.page("Germany").html()
-    return render_template("main.twig.html", page=page_py)
+    if request.method == 'POST':
+        ip_address = request.remote_addr
+        data = request.form
+        print(data)
+        pseudo = data["inputPseudo"]
+
+        with open('players.json', 'r') as f:
+            json_dict = json.load(f)
+
+        players = json_dict
+
+        print(players)
+
+        if ip_address not in players:
+            players[ip_address] = pseudo
+            with open('players.json', 'w') as outfile:
+                json.dump(players, outfile)
+
+            wikipedia.set_lang("fr")
+            page_py = wikipedia.page("Germany").html()
+            return render_template("main.html", page=page_py, username=pseudo, started_from="depart", target="Hitler")
+        else:
+            return render_template("connection.html", error="")
+    if request.method == 'GET':
+        return render_template("connection.html", error="")
+
 
 @app.route("/wiki/<title>")
 def next(title):
-    print(title)
+    ip_address = request.remote_addr
+    with open('players.json', 'r') as f:
+        json_dict = json.load(f)
+    players = json_dict
+    username = players[ip_address]
     wikipedia.set_lang("fr")
-    page_py = wikipedia.page(title, auto_suggest=False).html()
-    return render_template("main.twig.html", page=page_py)
+    page_py = wikipedia.page(title, auto_suggest=True).html()
+    return render_template("main.html", page=page_py, username=username, started_from="depart", target="Hitler")
 
-ui.run()                           # call the 'run' method 
+ui.run()
