1b541159e7aafa8031128c33d9b36973c56b2e17
Pelagoss <pelagoss@outlook.fr>
2023-06-27 20:08:56 +0200

    add .env

--- server/api/config.py
+++ server/api/config.py
@@ -1,6 +1,20 @@
+from datetime import datetime, timedelta
+import os
+
+import jwt
+from dotenv import load_dotenv, dotenv_values
+
+env = os.getenv('APP_ENV', 'local')
+dotenv_path = os.path.join(os.getcwd(), f'.env.{env}')
+
+## Load .env file
+load_dotenv()
+## Load .env.{APP_ENV}
+load_dotenv(dotenv_path, override=True)
+
 class BaseConfig(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'sqlite:///wikiwars.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # used for encryption and session management
-    SECRET_KEY = b'\xd4\xbd\x15\x84:U\xf5\xec\xf9\xcd"\x8d\xa6^lx'
+    SECRET_KEY = bytes(os.getenv("SECRET"), "latin-1")
