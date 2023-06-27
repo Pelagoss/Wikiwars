a346671494562ec5f0ba67804cc332432914d085
Pelagoss <pelagoss@outlook.fr>
2023-06-27 21:42:30 +0200

    .env

--- server/api/config.py
+++ server/api/config.py
@@ -1,20 +1,21 @@
-from datetime import datetime, timedelta
 import os
 
-import jwt
 from dotenv import load_dotenv, dotenv_values
 
-env = os.getenv('APP_ENV', 'local')
+env = os.getenv('ENV', 'local')
 dotenv_path = os.path.join(os.getcwd(), f'.env.{env}')
 
 ## Load .env file
-load_dotenv()
-## Load .env.{APP_ENV}
-load_dotenv(dotenv_path, override=True)
-
-class BaseConfig(object):
-    DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///wikiwars.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+configGlobal = dotenv_values()
+## Load .env.{ENV}
+config = dotenv_values(dotenv_path) | configGlobal
+config = config | {
+    'ENV': env,
+    'DEBUG': True,
+    'SQLALCHEMY_DATABASE_URI': 'sqlite:///wikiwars.db',
+    'SQLALCHEMY_TRACK_MODIFICATIONS': False,
     # used for encryption and session management
-    SECRET_KEY = bytes(os.getenv("SECRET"), "latin-1")
+    'SECRET_KEY': bytes(config.get('SECRET_KEY'), "latin-1")
+}
+
+BaseConfig = type('BaseConfig', (object,), config)
