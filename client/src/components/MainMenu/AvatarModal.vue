92324f5110b7b401c8ca94003768d3395ea363a3
Cyril HERRERA <cherrera@com-company.fr>
2023-08-31 14:08:01 +0200

    set lockable avatar

--- client/src/components/MainMenu/AvatarModal.vue
+++ client/src/components/MainMenu/AvatarModal.vue
@@ -2,11 +2,17 @@
     <Modal @close="close" persistent :closable="true" height="50%" width="30%" v-model="show">
         <div v-if="loading === false" class="p-8 flex flex-col justify-between h-full">
             <div class="grid grid-cols-6 gap-4">
-                <div v-for="a in avatars" @click="avatarSelected = a"
-                :class="{'locked': a.isUnlocked === false}">
-                    <img class="h-16 w-16 object-cover cursor-pointer rounded-lg border-4 hover:border-accent50 hover:scale-110"
-                         :class="[{'!border-accent': avatarSelected.id === a.id}]"
+                <div v-for="a in [...avatars.filter(a => a.isUnlocked === true), ...avatars.filter(a => a.isUnlocked === false)]"
+                     @click="a.isUnlocked === true ? avatarSelected = a : $event.preventDefault()"
+                     class="rounded-lg border-4 hover:border-accent50 hover:scale-110"
+                     :class="[{'locked': a.isUnlocked === false}, {'!border-accent': avatarSelected.id === a.id}]">
+
+                    <img class="rounded h-auto w-full aspect-square object-cover cursor-pointer"
                          :src="a.path">
+
+                    <div v-if="a.isUnlocked === false">
+                        <icone-dynamique-composant icon="LockClosed" type="outline" class="text-gray-400 !w-1/2 !h-auto"></icone-dynamique-composant>
+                    </div>
                 </div>
                 <!-- todo: show different avatar to choose the one for profile -->
             </div>
@@ -31,10 +37,11 @@ import Modal from "@/components/ui/Modal.vue";
 import {mapState} from "pinia";
 import {userStore} from "@/store/index.js";
 import Button from "@/components/ui/Button.vue";
+import IconeDynamiqueComposant from "@/components/IconeDynamiqueComposant.vue";
 
 export default {
     name: "AvatarModal",
-    components: {Button, Modal, SaberLoader},
+    components: {IconeDynamiqueComposant, Button, Modal, SaberLoader},
     props: {
         showModalAvatar: {
             type: Boolean
@@ -92,8 +99,13 @@ export default {
 
 .locked {
     @apply pointer-events-none relative;
-    mask: url(/images/lock.svg) center/contain no-repeat, linear-gradient(#000 0 0);
-    mask-composite: exclude;
-    mask-size: 100%;
+    & > div {
+        @apply absolute inset-0 flex center h-full w-full;
+        background-color: rgba(0, 0, 0, 0.45);
+    }
+
+    & > img {
+        @apply blur-[2px];
+    }
 }
 </style>
--- server/migrations/versions/eba9ac11a8e8_.py
+++ server/migrations/versions/eba9ac11a8e8_.py
@@ -64,7 +64,7 @@ def upgrade():
     idAvatar = ret.lastrowid
 
     idAvatars = [idAvatar]
-    for i in [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]:
+    for i in [1, 4, 5, 7, 10, 11]:
         data = {
             "path": f'design{i}.png',
             "condition_type": "free",
@@ -72,6 +72,14 @@ def upgrade():
         }
         ret = session.execute(insert(avatar).values(data))
         idAvatars.append(ret.lastrowid)
+
+    for i in [2, 6, 8, 9, 12, 13]:
+            data = {
+                "path": f'design{i}.png',
+                "condition_type": "lock",
+                "condition_value": None
+            }
+            ret = session.execute(insert(avatar).values(data))
 
     for u in session.execute(select(users)):
         for idA in idAvatars:
