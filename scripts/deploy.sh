f6cba6a6acb1f305032918ea441764ddff7c7650
Pelagoss <pelagoss@outlook.fr>
2023-11-13 01:06:46 +0100

    fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sed 's/$SQL_PASSWORD/'"$SQL_PASSWORD"'/' -i .env.prod.db
-sed 's/$SQL_PASSWORD/'"$SQL_PASSWORD"'/' -i /server/.env.prod
-sed 's/$MAIL_PASSWORD/'"$MAIL_PASSWORD"'/' -i /server/.env.prod
+sed 's/$SQL_PASSWORD/'"$SQL_PASSWORD"'/' -i server/.env.prod
+sed 's/$MAIL_PASSWORD/'"$MAIL_PASSWORD"'/' -i server/.env.prod
 sed 's/${ENV}/'"$ENV"'/' -i docker-compose.yml
 
-// Rename docker-compose.prod.yml
+# Rename docker-compose.prod.yml
 mv docker-compose.prod.yml docker-compose.override.yml
