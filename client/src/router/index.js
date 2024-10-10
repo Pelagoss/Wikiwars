868779d659ead9e81a31161a70ae14753e8e84c1
Pelagoss <pelagoss@outlook.fr>
2024-10-10 13:18:25 +0200

    Update index.js

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -14,6 +14,7 @@ const routes = [
         path: '/',
         name: 'accueil',
         component: Accueil,
+        redirect: {name: 'home'},
         children: [
             {
                 path: '',
